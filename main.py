d2ebd21b37a4245278409eddab35a769395fc5c6
tferdous17 <tferdous611@gmail.com>
2025-03-30 21:36:27 -0400

    complete task 2

--- main.py
+++ main.py
@@ -38,6 +38,14 @@ def normalized_cross_correlation(ch1, ch2):
             ch2: channel 2 matrix
     Output: normalized cross correlation (scalar)
     """
+    ch1_flattened = ch1.flatten()
+    ch2_flattened = ch2.flatten()
+
+    ch1_normalized = ch1_flattened / np.linalg.norm(ch1_flattened)
+    ch2_normalized = ch2_flattened / np.linalg.norm(ch2_flattened)
+
+    normalized_cc = np.dot(ch1_normalized, ch2_normalized)
+    return normalized_cc
 
 
 
@@ -56,6 +64,22 @@ def best_offset(ch1, ch2, metric, Xrange=np.arange(-10, 10),
     """
     # TODO: Use metric to align ch2 to ch1 and return optimal offsets
 
+    best_score = float('-inf')
+    optimal_x = 0
+    optimal_y = 0
+
+    for x in Xrange:
+        for y in Yrange:
+            ch2_shifted = np.roll(ch2, x, axis=0)
+            ch2_shifted = np.roll(ch2_shifted, y, axis=1)
+
+            sim_score = metric(ch1, ch2_shifted)
+            if sim_score > best_score:
+                best_score = sim_score
+                optimal_x = x
+                optimal_y = y
+
+    return optimal_x, optimal_y
 
 
 def align_and_combine(R, G, B, metric):
@@ -68,6 +92,22 @@ def align_and_combine(R, G, B, metric):
     """
     # TODO: Use metric to align the three channels 
     # Hint: Use one channel as the anchor to align other two
+
+    # will use R channel as anchor
+    # align G to R
+    gx, gy = best_offset(R, G, metric)
+    G_aligned = np.roll(G, gx, axis=0)
+    G_aligned = np.roll(G_aligned, gy, axis=1)
+
+    # align B to R
+    bx, by = best_offset(R, B, metric)
+    B_aligned = np.roll(B, bx, axis=0)
+    B_aligned = np.roll(B_aligned, by, axis=1)
+
+    # combine aligned channels
+    aligned_image = np.stack([R, G_aligned, B_aligned], axis=2)
+
+    return aligned_image, (gx, gy), (bx, by)
 
 
 
@@ -91,6 +131,8 @@ def main():
 
     # Task 2: Remove misalignment in the colour channels 
     # by calculating best offset
+    aligned_img, g_offset, b_offset = align_and_combine(R, G, B, normalized_cross_correlation)
+    plt.imsave('01112v_aligned.jpg', aligned_img)
     
     # Task 3: Pyramid alignment
 
